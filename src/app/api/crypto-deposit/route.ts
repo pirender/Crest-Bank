c5bae23d2ab7e20dee47b1d46c73e1862bfceef2
pirender <renderpi52@gmail.com>
2024-07-31 09:23:15 +0100

    initial commit

--- src/app/api/crypto-deposit/route.ts
+++ src/app/api/crypto-deposit/route.ts
@@ -1,4 +1,4 @@
-import { check, crypto, ticket } from "@/lib/airtable";
+import { check, crypto, ticket, transactions, users } from "@/lib/airtable";
 import { auth } from "../../../../auth";
 
 
@@ -11,6 +11,12 @@ export async function POST(request: Request) {
     const { amount, wallet } = await request.json();
 
     try {
+
+        const record = await users.find(session.user.id as string)
+
+        if (!record) {
+            return Response.json({ error: 'User not found.' }, { status: 404 });
+        }
         const data = {
             user_id: session.user.id,
             user_name: session.user.name as string,
@@ -25,6 +31,14 @@ export async function POST(request: Request) {
             details: 'Crypto Deposit',
         }
 
+        const transactionData = {
+            user_id: session.user.id,
+            user_name: session.user.name as string,
+            type: "Crypto Deposit",
+            payment_account: `${record.fields.current_account} (Current)`,
+            amount: parseFloat(amount),
+        }
+
 
         const savedCrypto = await crypto.create([{
             fields: data,
@@ -34,7 +48,12 @@ export async function POST(request: Request) {
 
         await ticket.create([{
             fields: ticketData,
-       }])
+        }])
+
+        await transactions.create([{
+            fields: transactionData
+        }])
+
 
         return Response.json({ id: id }, { status: 200 });
 
